6a715f12a4dc80bd34bdd691e7659bbd82a145ab
Ryan Whitmire <ryanw51@gmail.com>
2015-12-06 12:52:27 -0500

    Update browser-module.js

--- browser-module.js
+++ browser-module.js
@@ -1,3 +1,11 @@
+/*!
+ * browser-module v1.0.0
+ * https://github.com/rwhitmire/browser-module
+ *
+ * Copyright 2015 Ryan Whitmire
+ * Released under the MIT license
+ */
+
 window.module = function(ns, cb) {
   var namespaces = ns.split('.');
   var current = window;
